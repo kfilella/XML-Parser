80754a132d18e77d587bc8d3ff19c36873c9c17e
Adrian Aguilar <adrian11_12@hotmail.com>
2014-01-12 12:12:48 -0500

    listas con todo agrupado de device

--- proyecto haskell/Proyecto.hs
+++ proyecto haskell/Proyecto.hs
@@ -29,18 +29,53 @@ cargararchivo arch = do
 				codigo <- readFile arch
 				return (lines codigo)
 				
-printflistanueva [] = return()
-printflistanueva (x:xs) = do
-				putStrLn x
-				printflistanueva xs
 				
+
 				
-printlista [] = return ()
-printlista (x:xs) = do
+--printflistanueva [] = return()
+--printflistanueva (x:xs) = do
+			--	let listp= splitOneOf("<>= \\\"") x     --AQUI YA C SEPARAN LOS DATOS Y C GUARDAN EN UNA LISTA
+				--imprimir listp
+				
+				--printflistanueva xs
+				
+imprimir []=return()
+imprimir (x:xs) = do
+			putStrLn x
+			imprimir xs
+	
+espacios ::[String]->[String]
+espacios []=[]
+espacios (x:xs)= do
+	if x==""
+	then []++espacios xs
+	else [x]++espacios xs
+		
+
+device [] =return()
+device x = do
+		 let l = splitOneOf("<>=/ \\\"") x --AQUI YA C SEPARAN LOS DATOS Y C GUARDAN EN UNA LISTA
+		 let listsinespacio= espacios l
+		 imprimir listsinespacio
+	     
+listdevic [] =return()
+listdevic (x:xs) = do
+				if isInfixOf "<group" x then do
+					device x
+				else if isInfixOf "<product" x then do
+						device x
+				else if isInfixOf "<capability" x then 
+						device x
+				else putStrLn ""
+				listdevic xs
+
+					
+lista [] = return ()
+lista (x:xs) = do
 				if isInfixOf "<devices>" x then do
-					printflistanueva xs
+					listdevic xs
 				else
-					printlista xs
+					lista xs
 				
 main = do 
 	putStrLn "Parseo de XML"
@@ -56,11 +91,16 @@ main = do
 	putStrLn "*************************************"
 	putStrLn ""
 	putStrLn ("Leyendo el archivo device.xml")
-	xml <- cargararchivo "test.xml"
-	printlista xml
+	xml <- cargararchivo "test1.xml"
+	lista xml
 	putStrLn "cargado de documento exitoso"
 
-	
+elimespacios ::[String]->[String]
+elimespacios [] = []
+elimespacios (x:xs)=do
+	if x==""
+	then []++elimespacios xs
+	else [x]++elimespacios xs
 	
 
 						
