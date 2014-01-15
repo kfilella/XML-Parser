75e574720b60a91a9cb15e1588ae832c66e6a2c7
Kevin Filella <kfilella@espol.edu.ec>
2014-01-15 11:22:29 -0500

    estructura llena

--- proyecto haskell/Proyecto.hs
+++ proyecto haskell/Proyecto.hs
@@ -8,18 +8,27 @@ import Data.List.Split
 
 -}
 
+
 data Device = Device { id_device :: String, 
                        user_agent :: String, 
                        fall_back :: String
                      } deriving (Eq,Show,Read)
 					 
-data Group = Group { id_group :: String
-                   } deriving (Eq,Show,Read)
+data Group = Group { --id_device_ref :: String
+						id_group :: String
+					} deriving (Eq,Show,Read)
 
-data Capability = Capability { name :: String,
-					value :: String
+data Capability = Capability { --id_group_ref :: String,
+					   name :: String, 
+                       value :: String
                      } deriving (Eq,Show,Read)
-					 					 			   
+					 
+					 
+				   
+data Grupo =Grupo {dev :: Device, 
+                    gp :: Group, 
+                     cp :: Capability
+					}deriving (Eq,Show,Read)
 
 	
 cargararchivo :: FilePath -> IO [String]
@@ -27,44 +36,32 @@ cargararchivo arch = do
 				codigo <- readFile arch
 				return (lines codigo)
 				
+				
+getIdDevice :: Device -> String
+getIdDevice (Device id ua fb) = id
+				
+				
 --printflistanueva [] = return()
 --printflistanueva (x:xs) = do
 			--	let listp= splitOneOf("<>= \\\"") x     --AQUI YA C SEPARAN LOS DATOS Y C GUARDAN EN UNA LISTA
 				--imprimir listp
 				
 				--printflistanueva xs
---buscarDevice [] id= return ()
---buscarDevice (a:b:c:d) id =do
---				if a==id then do
-	--				if c=="fall_back" then do
-		--			putStrLn "ID:"++a++"user_agent:  fall_back:"++d 
-			--		else putStrLn ""
-						--putStrLn "ID:"++a++"user_agent:"++c++"fall_back:"++d
-			   -- else putStrLn ""
 				
-				
-imprimir [] car  =return()
-imprimir (x:xs) car  =  do
-			if x=="name" then do	
-				if  (head xs)==car	then do					
-				putStrLn  (head xs)				
-				else imprimir (xs) car
-			else imprimir (xs) car
-			--lc++1
-			--putStrLn (length lc)
-			 
-			--	buscarDevice sl ido
-		--	if x=="device"then do
-		--		 if y== "id" then do
-			--		 let ido = (head z)
-			--		else print ""
-			--	else print ""
-				
-			
-			
-printDevice :: Device -> String
-printDevice(Device id user fall)="ID: "++id++" User Agent: "++user++" Fall Back: "++fall
+imprimir []=return()
+imprimir (x:xs) = do
+			putStrLn x
+			imprimir xs
 
+impridevice :: Device -> String
+impridevice (Device id u f) = "Device: id-> "++id++" user_agent->"++u++" fall_back->"++f
+
+imprigroup :: Group-> String
+imprigroup(Group id)= "Group: id="++id
+
+impricapability :: Capability -> String
+impricapability(Capability n v)= "Capability: name= "++n++" value="++v
+	
 espacios ::[String]->[String]
 espacios []=[]
 espacios (x:xs)= do
@@ -72,91 +69,144 @@ espacios (x:xs)= do
 	then []++espacios xs
 	else [x]++espacios xs
 	
---buscaCar :: [String]->  String-> IO ()
---buscaCar [] car = putStrLn "no encontrado"
---buscaCar (x:xs) car	= do
-		--	if (x==car) then do
-		--	putStrLn "encontrado"			
-		--	else buscaCar xs car
-			
-			
-creardevice :: [String] -> Device
-creardevice [] = Device "" "" ""
-creardevice (a:b:c:d:e) = do	
-			let k = "none"
-			if(d=="fall_back") then do
-			 Device b k (head e)
-			else  Device b d (last e)
-			
+listacapability[]=[]
+listacapability (x:xs) = do
+		if (x=="name") then do
+			[head xs]++listacapability xs
+		else if (x=="value") then do
+			if (xs==[]) then do
+				xs++["none"]
+				else [head xs]++listacapability xs
+		else listacapability xs
 
-crearcapability :: [String] -> Capability
-crearcapability [] = Capability "" ""
-crearcapability (a:b:c) = do	
-			let k = "none"
-			if((last c)=="value") then do
-			 Capability b k
-			else  Capability b (last c)
-			
-			
-creargroup :: [String] -> Group
-creargroup [] = Group ""
-creargroup a = do
-			 Group (last a)
-		
+
+listagroup[]=[]
+listagroup (x:xs) = do
+		if (x=="id") then do
+			[head xs]++listagroup xs
+		else listagroup xs
 	
-	
-		
-device [] car [] [] [] = return()
-device x car ld lg lc = do
-		 let l = splitOneOf("<>=/ \\\"") x --AQUI YA C SEPARAN LOS DATOS Y C GUARDAN EN UNA LISTA
-		 let listsinespacio= espacios l		 
-		 let devcc=creardevice(["id","kljh","user_agent","fall_back","tttt"])
-		 listadevices++[devcc]
-		 imprimir listsinespacio car
-		 --if (head listsinespacio)=="device" then do
-			--[creardevice(tail listsinespacio)++ld
-			--else print "no se pudo"
-		 --if (head listsinespacio)=="group" then do
-			--lg++creargroup(tail listsinespacio)
-			--print ""
-			--else print ""
-		 --if (head listsinespacio)=="capability" then do
-			--lc++crearcapability(tail listsinespacio)
-			--print ""
-			--else print ""
-
-
-
+listadevice ::[String] -> [String]	
+listadevice[]=[]
+listadevice (x:xs) = do
+		if (x=="id") then do
+			[head xs]++listadevice xs
+		else if (x=="user_agent")then do
+		    [head xs]++listadevice xs
+			--if(head xs == "fall_back") then do
+			--	[x]++ ""
+			--else [head xs]++listadevice xs
+		else if (x=="fall_back") then do
+			[head xs]++listadevice xs
+		else listadevice xs
 			
-listdevic [] car [] [] []= return()
-listdevic (x:xs) car ld lg lc= do
+		
+		
+createdevice ::[String] -> Device
+createdevice [] = Device "" "" ""
+createdevice (x:y:zs) = do
+	Device x y (head zs)
+	
+createcapability [] = Capability "" ""
+createcapability (x:ys) = do
+	Capability x (head ys)
+
+
+creategroup [] = Group ""
+creategroup (x:ys) = do
+	Group x
+
+	
+creategrupo ::Device->Group->Capability->Grupo
+creategrupo (Device "" "" "") (Group "") (Capability "" "") = Grupo (Device "" "" "") (Group "") (Capability "" "")
+creategrupo dev gp cp = do
+	Grupo dev gp cp
+
+sacardevice ::Grupo -> Device
+sacardevice (Grupo (Device "" "" "") (Group "") (Capability "" ""))= (Device "" "" "") 
+sacardevice grp = do
+	dev grp
+	
+sacargroup ::Grupo -> Group
+sacargroup (Grupo (Device "" "" "") (Group "") (Capability "" ""))= (Group "") 
+sacargroup grp = do
+	gp grp
+
+sacarcapab ::Grupo -> Capability
+sacarcapab (Grupo (Device "" "" "") (Group "") (Capability "" ""))= (Capability "" "") 
+sacarcapab grp = do
+	cp grp
+	
+funcioncapabi x  = do
+			let list = listacapability x
+			if(list/=[]) then createcapability list
+			else Capability "" ""
+
+funciongroup x  = do
+			let list = listagroup x
+			if(list/=[]) then creategroup list
+			else Group ""
+	
+--createdevice ::[String] -> Device	
+funciondevice::[String]	-> Device	
+funciondevice	x  = do
+			let list = listadevice x
+			if(list/=[]) then createdevice list
+			else Device "" "" ""
+			
+devicedecapab :: Grupo -> String
+devicedecapab grp = do
+	impridevice (sacardevice grp)
+	
+
+	
+		--AQUI SE DEBE HACER TODO
+prodt ::[String]->Device->Group->Capability->[Grupo]
+prodt [] _ _ _ =[]
+prodt (x:xs) dev gp cp = do
+	let l = splitOneOf("<>=/ \\\"") x --AQUI YA C SEPARAN LOS DATOS Y C GUARDAN EN UNA LISTA
+	let listsinespacio= espacios l  --- lista de le linea sin espacios
+	let lista=[]
+	if (head listsinespacio) == "device" then do
+			let devi = funciondevice (tail listsinespacio)
+			--putStrLn (impridevice devi)
+			[]++prodt xs dev gp cp
+	else if (head listsinespacio) == "group" then do
+			let grou = funciongroup (tail listsinespacio)
+			--let idDe = getDevice dev
+			--putStrLn (imprigroup grou)
+			[]++prodt xs dev gp cp
+	else if (head listsinespacio) == "capability" then do
+			let capa = funcioncapabi (tail listsinespacio)
+			let grupo = creategrupo dev gp capa
+			--putStrLn (impricapability  capa)
+			[grupo]++prodt xs dev gp cp
+	else lista++prodt xs dev gp cp
+	--putStrLn impridevice devi
+	--imprimir listsinespacio
+	     
+{-listdevic [] =return()
+listdevic (x:xs) = do
+				let lista=[]
 				if isInfixOf "<device" x then do
-					device x car ld lg lc
-					listdevic xs car ld lg lc
+					lista ++ prodt x 
+					
 				else if isInfixOf "<group" x then do
-						device x car ld lg lc
-						listdevic xs car ld lg lc
-				else if isInfixOf "<capability" x then do
-						device x car ld lg lc
-						listdevic xs car ld lg lc
-				else listdevic xs car ld lg lc
-				putStrLn ""
-				
-				
-				
-				
-				
-
-lista [] car = return ()
-lista (x:xs) car = do
-				let listadevices = []
-				let listagroups = []
-				let listacapabilities = []
+					lista ++ prodt x 
+					
+				else if isInfixOf "<capability" x then 
+					lista ++ prodt x 
+					
+				else putStrLn " "
+				listdevic xs
+-}
+					
+lista :: [String]->[Grupo]
+lista (x:xs) = do
 				if isInfixOf "<devices>" x then do
-					listdevic xs car listadevices listagroups listacapabilities
+					prodt xs (Device "" "" "") (Group "") (Capability "" "")
 				else
-					lista xs car
-				
+					lista xs
 				
 main = do 
 	putStrLn "Parseo de XML"
@@ -173,36 +223,16 @@ main = do
 	putStrLn ""
 	putStrLn ("Leyendo el archivo device.xml")
 	xml <- cargararchivo "test1.xml"
-	putStrLn ("Ingrese la caracteristica o capability que desea consultar")
-	caracteristica <- getLine
-	putStrLn "estos son todas las capabilities "	
-	lista xml caracteristica
+	let lis = lista xml
 	putStrLn "cargado de documento exitoso"
-	--menu1 xml
+
+elimespacios ::[String]->[String]
+elimespacios [] = []
+elimespacios (x:xs)=do
+	if x==""
+	then []++elimespacios xs
+	else [x]++elimespacios xs
 	
-	
-	
---menu1 :: [String] -> IO ()
---menu1 [] = return ()
---menu1 (x:xs) = do
-			--	if isInfixOf "<device" x 
-			--	then do
-				--		putStrLn ""
-				--		
-				--		putStrLn "1 -- Nombre(ID) del device "
-				--		putStrLn "2 -- Capability del device"
-				--		opcion <- getLine
-				--		if opcion == "1"
-				--		then do putStrLn "escriba el  Nombre(ID) del device "
-				--		        nombre <- getLine
-				--		        devicesConNombre nombre
-				--		else if number == "2"
-				--		then do putStrLn "escriba la capability del device"
-				--		        capability <- getLine
-				--				capabilityDevices capability
-				--			   else do putStrLn "opcion no valida"
-							 
+
 						
 						
-				--else
-				--	menu1 xs
